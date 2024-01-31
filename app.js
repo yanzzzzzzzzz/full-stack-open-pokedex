8ba55e6c3875142d60f693a79b0a4fc958a028a3
Jack Tsai <jtsai@nexify.com.hk>
2024-01-31 12:25:41 +0800

    .

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.listen(PORT, () => {
+app.listen({
+  port: PORT,
+  host: '0.0.0.0'
+}, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
 })
